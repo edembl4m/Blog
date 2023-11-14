21ee3ba78d3aa0403327b80a185deea0c43d2c57
edembl4m <mametov.edem@bk.ru>
2023-11-14 18:36:38 +0300

    post list update animation

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import PostItem from "./PostItem";
 
 const PostList = ({posts, title, remove}) => {
@@ -12,9 +13,17 @@ const PostList = ({posts, title, remove}) => {
   return (
     <div>
         <h1 style={{textAlign: 'center'}}>{title}</h1>
+        <TransitionGroup>
             {posts.map((post, index) => 
-                <PostItem remove={remove} number={index + 1} post={post} key={post.id}/>
+                <CSSTransition
+                    key={post.id}
+                    timeout={500}
+                    classNames='post'
+                >
+                    <PostItem remove={remove} number={index + 1} post={post}/>
+                </CSSTransition>
             )}
+        </TransitionGroup>
     </div>
   )
 }
